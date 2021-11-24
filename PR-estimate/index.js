e3dd08cda32ee255894d5e569816e0862de84e24
Logan <lsaunders275@gmail.com>
2021-11-23 21:29:27 -0700

    finished, i think

--- PR-estimate/index.js
+++ PR-estimate/index.js
@@ -10,3 +10,16 @@ const addBoxShadowToRange = () => {
   document.head.appendChild(style)
 }
 addBoxShadowToRange()
+
+document.querySelector('form').addEventListener('submit', (e) => {
+  e.preventDefault()
+  const [range, linesAdded, filesChanged] = [...e.target].map((el) => el.value)
+  const complexity = range / 10
+  const estimate = parseInt((((Math.pow(complexity, 2) * linesAdded) + Math.sqrt(filesChanged)) * .01).toFixed(2), 10)
+  
+  document.querySelector('#approx').innerText = estimate
+  document.querySelector('#min').innerText = estimate <= 0 ? 0 : estimate - 2
+  document.querySelector('#max').innerText = Math.floor(estimate + 2)
+
+  document.querySelector('main').classList.add('slide')
+})
